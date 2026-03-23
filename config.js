0d656afcc46a56cffe169ccc884913189e4651f2
daqbang-dot <daqbang@gmail.com>
2026-03-24 00:30:59 +0800

    Delete config.js

--- config.js
+++
@@ -1,6 +0,0 @@
-const GITHUB_CONFIG = {
-    TOKEN: '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',
-    REPO_OWNER: 'daqbang-dot',
-    REPO_NAME: 'NotepadGit',
-    FILE_PATH: 'data/notes.json'
-};
